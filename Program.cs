9d4a0b05db4ae3b19fc29ba3790d112be8b09a4e
Gonzalo Sandoval <sandovalvidalgonzalo@gmail.com>
2026-09-23 12:36:39 -0500

    Impresion de notas

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//Funcion de impresion de notas
+static void ImprimirNotas(int[] notasx)
+{
+    for(int i = 0; i < notasx.Length; i++)
+    {
+        Console.WriteLine($"nota {i+1}: {notasx[i]}");
+    }
+}
+
+int[] notas = {15,18,12,17,20};
+
+//imprimir el arreglo de notas
+ImprimirNotas(notas);
+
+static int SumarNotas(int[] notasx)
+{
+    int suma = 0;
+    for(int i = 0; i < notasx.Length; i++)
+    {
+        suma += notasx[i];
+    }
+    return suma;
+}
+
+int sumaTotal = SumarNotas(notas);
+Console.WriteLine($"Suma total: {sumaTotal}");
+
+
